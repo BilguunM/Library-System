8e3c11291e3959f2d3eb3a8021d74224f317ddf1
chambaii <45263036+chambaii@users.noreply.github.com>
2018-11-22 22:11:37 +0800

    Add files via upload

---
+++ student.java
@@ -0,0 +1,62 @@
+package sw_sem7;
+
+public class student {
+	private String st_code;
+	private String l_name;
+	private String f_name;
+	private String address;
+	private int phoneNum;
+	
+	public student() {
+		
+	}
+	
+	public student(String id, String lname, String fname, String address, int phoneNum) {
+		this.st_code = id;
+		this.l_name = lname;
+		this.f_name = fname;
+		this.address = address;
+		this.phoneNum = phoneNum;
+	}
+	
+	public String getSt_code() {
+		return st_code;
+	}
+
+	public void setSt_code(String st_code) {
+		this.st_code = st_code;
+	}
+
+	public String getL_name() {
+		return l_name;
+	}
+
+	public void setL_name(String l_name) {
+		this.l_name = l_name;
+	}
+
+	public String getF_name() {
+		return f_name;
+	}
+
+	public void setF_name(String f_name) {
+		this.f_name = f_name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(int phoneNum) {
+		this.phoneNum = phoneNum;
+	}	
+
+}
